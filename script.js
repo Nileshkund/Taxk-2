b1fb430a8a182207cd3b2398fb70699b5a5e5b80
NILESH KUND <111678618+Nileshkund@users.noreply.github.com>
2023-12-30 06:30:16 +0000

    com

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-console.log('====================================');
-console.log("Connected");
-console.log('====================================');
+function proceedToNextPage() {
+   
+    var checkoutButton = document.querySelector('.checkout-button');
+    checkoutButton.innerHTML = 'Purchase History';
+  
+    
+    checkoutButton.disabled = true;
+    showOrderReceivedMessage();
+    
+  }
+  
+  function showOrderReceivedMessage() {
+    
+    var messageElement = document.createElement('p');
+    messageElement.innerHTML = '<strong>Your order has been received</strong>';
+    var cardElement = document.querySelector('.card');
+    cardElement.insertBefore(messageElement, cardElement.firstChild);
+  }
+  
